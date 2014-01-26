ee67a896506787185126ef90c99eaceaad086e9e
hughsando <git@nmehost>
2014-01-26 22:58:05 +0800

    New nme building code

--- src/NMEBuilder.hx
+++ src/NMEBuilder.hx
@@ -23,42 +23,37 @@ class NMEBuilder extends Builder
 
       if (inBinary=="windows")
       {
-         command("haxelib", ["run", "hxcpp", "Build.xml" ]);
+         command("neko", ["builder.n", inBinary ]);
          Sys.setCwd(dir);
          command("tar", ["cvzf", "nme-bin-windows.tgz", "ndll/Windows"]);
       }
       else if (inBinary=="mac")
       {
-         command("haxelib", ["run", "hxcpp", "Build.xml"]);
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-DHXCPP_M64"]);
+         command("neko", ["builder.n", inBinary ]);
          Sys.setCwd(dir);
          command("tar", ["cvzf", "nme-bin-mac.tgz", "ndll/Mac", "ndll/Mac64"]);
       }
       else if (inBinary=="ios")
       {
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-Diphoneos"]);
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-Diphoneos", "-DHXCPP_ARMV7"]);
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-Diphonesim"]);
+         command("neko", ["builder.n", inBinary ]);
          Sys.setCwd(dir);
          command("tar", ["cvzf", "nme-bin-ios.tgz", "ndll/IPhone"]);
       }
       else if (inBinary=="android")
       {
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-Dandroid"]);
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-Dandroid", "-DHXCPP_ARMV7"]);
+         command("neko", ["builder.n", inBinary ]);
          Sys.setCwd(dir);
          command("tar", ["cvzf", "nme-bin-android.tgz", "ndll/Android"]);
       }
       else if (inBinary=="linux")
       {
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-Dlinux"]);
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-Dlinux", "-DHXCPP_M64"]);
+         command("neko", ["builder.n", inBinary ]);
          Sys.setCwd(dir);
          command("tar", ["cvzf", "nme-bin-linux.tgz", "ndll/Linux", "ndll/Linux64" ]);
       }
       else if (inBinary=="rpi")
       {
-         command("haxelib", ["run", "hxcpp", "Build.xml", "-Drpi"]);
+         command("neko", ["builder.n", inBinary ]);
          Sys.setCwd(dir);
          command("tar", ["cvzf", "nme-bin-rpi.tgz", "ndll/RPi"]);
       }
