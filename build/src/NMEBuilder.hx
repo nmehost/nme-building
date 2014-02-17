36df081a348de5d92670d8b24394cf2c1cca0372
hughsando <git@nmehost>
2014-02-18 00:01:06 +0800

    Zip libs too

--- build/src/NMEBuilder.hx
+++ build/src/NMEBuilder.hx
@@ -23,37 +23,37 @@ class NMEBuilder extends Builder
 
       if (inBinary=="windows")
       {
-         command("neko", ["builder.n", inBinary ]);
+         command("neko", ["build.n", inBinary ]);
          Sys.setCwd(dir);
-         command("tar", ["cvzf", "nme-bin-windows.tgz", "ndll/Windows"]);
+         command("tar", ["cvzf", "nme-bin-windows.tgz", "ndll/Windows", "lib/Windows"]);
       }
       else if (inBinary=="mac")
       {
-         command("neko", ["builder.n", inBinary ]);
+         command("neko", ["build.n", inBinary ]);
          Sys.setCwd(dir);
-         command("tar", ["cvzf", "nme-bin-mac.tgz", "ndll/Mac", "ndll/Mac64"]);
+         command("tar", ["cvzf", "nme-bin-mac.tgz", "ndll/Mac", "ndll/Mac64", "lib/Mac", "lib/Mac64"]);
       }
       else if (inBinary=="ios")
       {
-         command("neko", ["builder.n", inBinary ]);
+         command("neko", ["build.n", inBinary ]);
          Sys.setCwd(dir);
-         command("tar", ["cvzf", "nme-bin-ios.tgz", "ndll/IPhone"]);
+         command("tar", ["cvzf", "nme-bin-ios.tgz", "lib/IPhone"]);
       }
       else if (inBinary=="android")
       {
-         command("neko", ["builder.n", inBinary ]);
+         command("neko", ["build.n", inBinary ]);
          Sys.setCwd(dir);
-         command("tar", ["cvzf", "nme-bin-android.tgz", "ndll/Android"]);
+         command("tar", ["cvzf", "nme-bin-android.tgz", "ndll/Android", "lib/Android"]);
       }
       else if (inBinary=="linux")
       {
-         command("neko", ["builder.n", inBinary ]);
+         command("neko", ["build.n", inBinary ]);
          Sys.setCwd(dir);
-         command("tar", ["cvzf", "nme-bin-linux.tgz", "ndll/Linux", "ndll/Linux64" ]);
+         command("tar", ["cvzf", "nme-bin-linux.tgz", "ndll/Linux", "ndll/Linux64", "lib/Linux", "lib/Linux64" ]);
       }
       else if (inBinary=="rpi")
       {
-         command("neko", ["builder.n", inBinary ]);
+         command("neko", ["build.n", inBinary ]);
          Sys.setCwd(dir);
          command("tar", ["cvzf", "nme-bin-rpi.tgz", "ndll/RPi"]);
       }
